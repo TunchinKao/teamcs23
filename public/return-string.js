0e7e019cc4b14e3ca95158f17efa716f46877526
LAPTOP-34ORDA4I\Akizuki <a3250766@gmail.com>
2020-10-25 13:40:04 +0800

    new string

--- public/return-string.js
+++ public/return-string.js
@@ -1,76 +1,94 @@
+const { EOF } = require('dns');
 var fs=require('fs');
+var sector=new Array();
+sector=data.toString();
 var twoDarray=new Array();
-
-const require_index = 2;
-const optimal_index = 3;
-var fileName;
-var limit; // 不知道怎麼測.txt長度的下下策
+var j=0;
 module.exports = function(g){
-    if(g.grade == 1)
-        return 'aaa';
-    else if(g.grade == 2){
-        fileName = 'CS_require_course.txt';
-        limit = 53;
-    }
-    else if(g.grade == 3){
-        fileName = 'CS_optimal_course.txt';
-        limit = 31;
-    }
-
-    fs.readFile(fileName.toString(),function(err, data){
-        if(err) throw err;
-    
-        var sector=new Array();
-        sector=data.toString();
-        var j=0;
-        for(var i=0;i<limit;i++){
-            // twoDarray[i]=new Array();
-            // j++;
-            twoDarray[i] = "";
-            for(j;sector[j]!='\n';j++){
-                twoDarray[i] += (sector[j]);
-            }
-            j++;
-            console.log(twoDarray[i]);
-        }
-        // console.log(twoDarray[52].join(''));
-    });
-    return twoDarray;
-};
-/*fs.readFile('CS_require_course.txt',function(err, data){
+fs.readFile('CS_require_course.txt',function(err, data){
     if(err) throw err;
 
-    var sector=new Array();
-    sector=data.toString();
-    var j=0;
-    for(var i=0;i<53;i++){
+    for(var i=0;i<54;i++){
         twoDarray[i]=new Array();
-        for(j;sector[j]!='\n';j++){
+        for(j;sector[j]!='\n'&&sector[j]!='\r';j++){
+            if(i==53&&j==sector.length)break;
             twoDarray[i].push(sector[j]);
         }
-        j++;
-        console.log(twoDarray[i]);
+        j+=2;
+        //console.log(twoDarray[i]);
     }
-    console.log(twoDarray[52].join(''));
-});*/
 
-// module.exports = function(g){
-//     if(g.grade == 2)return 'aaa';
-//     fs.readFile('CS_require_course.txt',function(err, data){
-//         if(err) throw err;
-    
-//         var sector=new Array();
-//         sector=data.toString();
-//         var j=0;
-//         for(var i=0;i<53;i++){
-//             twoDarray[i]=new Array();
-//             for(j;sector[j]!='\n';j++){
-//                 twoDarray[i].push(sector[j]);
-//             }
-//             j++;
-//             console.log(twoDarray[i]);
-//         }
-//         console.log(twoDarray[52].join(''));
-//     });
-//     return twoDarray;
-// };
+    var map=new Array(9);
+    var maprow=0;
+    for(var i=0;i<54;i++){
+        if((twoDarray[i].join(''))=="大一上"){
+            map[maprow]=new Array();
+            for(var j=i+2;;j++){
+                if((twoDarray[j].join('')=="------")||((twoDarray[j].join('')=="---")))break;
+                map[maprow].push(twoDarray[j].join(''));
+            }
+            console.log(map[maprow]);
+            maprow++;
+        }
+        else if((twoDarray[i].join(''))=="大一下"){
+            map[maprow]=new Array();
+            for(var j=i+2;;j++){
+                if((twoDarray[j].join('')=="------")||((twoDarray[j].join('')=="---")))break;
+                map[maprow].push(twoDarray[j].join(''));
+            }
+            console.log(map[maprow]);
+            maprow++;
+        }
+        else if((twoDarray[i].join(''))=="大二上"){
+            map[maprow]=new Array();
+            for(var j=i+2;;j++){
+                if((twoDarray[j].join('')=="------")||((twoDarray[j].join('')=="---")))break;
+                map[maprow].push(twoDarray[j].join(''));
+            }
+            console.log(map[maprow]);
+            maprow++;
+        }
+        else if((twoDarray[i].join(''))=="大二下"){
+            map[maprow]=new Array();
+            for(var j=i+2;;j++){
+                if((twoDarray[j].join('')=="------")||((twoDarray[j].join('')=="---")))break;
+                map[maprow].push(twoDarray[j].join(''));
+            }
+            console.log(map[maprow]);
+            maprow++;
+        }
+        else if((twoDarray[i].join(''))=="大三上"){
+            map[maprow]=new Array();
+            for(var j=i+2;;j++){
+                if((twoDarray[j].join('')=="------")||((twoDarray[j].join('')=="---")))break;
+                map[maprow].push(twoDarray[j].join(''));
+            }
+            console.log(map[maprow]);
+            maprow++;
+        }
+        else if((twoDarray[i].join(''))=="大三下"){
+            map[maprow]=new Array();
+            for(var j=i+2;;j++){
+                if((twoDarray[j].join('')=="------")||((twoDarray[j].join('')=="---")))break;
+                map[maprow].push(twoDarray[j].join(''));
+            }
+            console.log(map[maprow]);
+            maprow++;
+        }
+        else if((twoDarray[i].join(''))=="大四上"){
+            map[maprow]=new Array();
+            for(var j=i+2;;j++){
+                if((twoDarray[j].join('')=="------")||(twoDarray[j].join('')=="---"))break;
+                map[maprow].push(twoDarray[j].join(''));
+            }
+            console.log(map[maprow]);
+            maprow++;
+        }
+    }
+    var space="";
+    map[maprow]=new Array();
+    map[maprow].push(space);
+    console.log(map[maprow]);
+});
+    return map[g.grade];
+}
